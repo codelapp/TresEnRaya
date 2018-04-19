93dbda2f43bf21340177b373462d3f8ac775e038
codelapp <info@codelapp.com>
2018-04-19 17:49:28 +0200

    Primer commit

--- TresEnRaya/AppDelegate.swift
+++ TresEnRaya/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  GaTO
-//
-//  Created by Jorge Maldonado Borbón on 05/11/17.
-//  Copyright © 2017 Jorge Maldonado Borbón. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
